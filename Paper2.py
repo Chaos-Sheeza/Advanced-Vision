2f3467bdc7f080552fc6ef47ce7ba84d946af32b
Chaos-Sheeza <jesmar16@live.com>
2019-11-02 20:55:38 +0100

    Added comments

--- Paper2.py
+++ Paper2.py
@@ -1,6 +1,3 @@
-#mosquito almost chocked me to death
-#jenny's tale
-
 import cv2
 
 T = cv2.imread("P1 Resources/c4t.bmp")
@@ -8,20 +5,31 @@ D = cv2.imread("P1 Resources/c4d.bmp")
 p1,p2 = [],[]
 s = 20
 
-def mouse(event,x,y,flags,param):
-    if event == cv2.EVENT_LBUTTONDBLCLK:
-        p1.clear()
-        p1.append(x)
-        p2.clear()
-        p2.append(y)
-        im2 = patch(p1[0],p2[0],T,s)
-        cv2.imshow('test',im2)
-        return True
+# Standard deviation function
+def SSD(refPatch, iPatch):
+        temp = refPatch-iPatch
+        ret = temp*temp
+        return ret
 
+# on mouse function to pass as a cv2.setMouseCallback() parameter.
+def mouse(event,x,y,flags,param):
+        if event == cv2.EVENT_LBUTTONDBLCLK:    # if left mouse button is double clicked
+                # clears previous coordinates and appends current mouse coordinate.
+                p1.clear()
+                p1.append(x)
+                p2.clear()
+                p2.append(y)
+                # grabs patch around mouse click and displays patch.
+                im2 = patch(p1[0],p2[0],T,s)
+                cv2.imshow('test',im2)
+                return True
+
+# function that returns a patch around a single coordinate.
 def patch(y,x,img, size):
+        # gets the maximum possible xy coordinate.
         maxY,maxX = img.shape[0], img.shape[1]
 
-        
+        # if statements to handle coordinates that go out of bounds.
         if x-(size/2) < 0:
                 newX = 0
         elif x+(size/2) > maxX-1:
@@ -36,22 +44,41 @@ def patch(y,x,img, size):
         else:
                 newY = y+(size/2)
         
+        # changes values from float to int
         newX = int(newX)
         newY = int(newY)
 
+        # grabing patch using numpy array
         patch = img[newX:newX+size, newY-size:newY, :]
         return patch
 
+# resource: "Efficient Object Selection using Depth and Texture Information", Dylan Seychell, Carl James Debono 
 '''
 def dtObj(xs,ys,treshD,treshT,T,D,Patch):
         x,y,i = 0,0,0
+        Ld,Lt = [],[]
+        #ssdD,ssdT = [],[]
         dPatch = patch(xs,ys,D,Patch)
         tPatch = patch(xs,ys,T,Patch)
+        while(x<(T.shape[1]-Patch) and y<(T.shape[0]-Patch)):
+
+                idPatch = patch(x,y,D,Patch)
+                itPatch = patch(x,y,T,Patch)
+                ssdD = SSD(dPatch,idPatch)
+                ssdT = SSD(tPatch,itPatch)
+
+                if ssdD < treshD:
+                        Ld.append(x,y)
+                if ssdT < treshT:
+                        Lt.append(x,y)
+
+                x = x + Patch
+                y = y + Patch
         return Ld, Lt
 '''
+
 cv2.imshow('Texture',T)
 #cv2.imshow('Depth',D)
 cv2.setMouseCallback('Texture', mouse)
 cv2.waitKey(0)
-#cv2.waitKey(0)
 cv2.destroyAllWindows
