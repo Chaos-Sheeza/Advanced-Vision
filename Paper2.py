1adb5478c73408664d6171b55bbe5372278b87b1
Chaos-Sheeza <jesmar16@live.com>
2019-11-08 18:17:01 +0100

    Fixed patches that returned zero, optimized patch function, and added treshold variables.

--- Paper2.py
+++ Paper2.py
@@ -1,15 +1,20 @@
 import cv2
+import numpy as np
 
 T = cv2.imread("P1 Resources/c4t.bmp")
 D = cv2.imread("P1 Resources/c4d.bmp")
+td = 60000
+tt = 30000
 p1,p2 = [],[]
 s = 20
 
 # Standard deviation function
 def SSD(refPatch, iPatch):
-        temp = refPatch-iPatch
-        ret = temp*temp
-        return ret
+        #print(refPatch)
+        #print(iPatch)
+        temp = np.sum(np.subtract(refPatch,iPatch)**2)
+        #ret = temp*temp
+        return temp
 
 # on mouse function to pass as a cv2.setMouseCallback() parameter.
 def mouse(event,x,y,flags,param):
@@ -22,9 +27,12 @@ def mouse(event,x,y,flags,param):
                 # grabs patch around mouse click and displays patch.
                 im2 = patch(p1[0],p2[0],T,s)
                 cv2.imshow('test',im2)
+                print(dtObj(x,y,tt,td,T,D,20)) # check cv2 tresholds.
                 return True
 
 # function that returns a patch around a single coordinate.
+
+'''
 def patch(y,x,img, size):
         # gets the maximum possible xy coordinate.
         maxY,maxX = img.shape[0], img.shape[1]
@@ -51,31 +59,37 @@ def patch(y,x,img, size):
         # grabing patch using numpy array
         patch = img[newX:newX+size, newY-size:newY, :]
         return patch
+'''
+
+def patch(y,x,img,size):
+        temp = int(size/2)
+        return img[x-temp:x+temp, y-temp:y+temp]
 
 # resource: "Efficient Object Selection using Depth and Texture Information", Dylan Seychell, Carl James Debono 
-'''
 def dtObj(xs,ys,treshD,treshT,T,D,Patch):
-        x,y,i = 0,0,0
+        x,y,i = 20,20,0
         Ld,Lt = [],[]
         #ssdD,ssdT = [],[]
         dPatch = patch(xs,ys,D,Patch)
         tPatch = patch(xs,ys,T,Patch)
         while(x<(T.shape[1]-Patch) and y<(T.shape[0]-Patch)):
-
                 idPatch = patch(x,y,D,Patch)
+                #print(idPatch)
                 itPatch = patch(x,y,T,Patch)
+                #print(itPatch)
                 ssdD = SSD(dPatch,idPatch)
                 ssdT = SSD(tPatch,itPatch)
+                print(ssdD)
 
                 if ssdD < treshD:
-                        Ld.append(x,y)
+                        Ld.append([x,y])
                 if ssdT < treshT:
-                        Lt.append(x,y)
+                        Lt.append([x,y])
 
                 x = x + Patch
                 y = y + Patch
         return Ld, Lt
-'''
+
 
 cv2.imshow('Texture',T)
 #cv2.imshow('Depth',D)
